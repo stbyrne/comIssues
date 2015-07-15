2d8643e336d07425ae2379f67e40ee66adb82113
stbyrne <strtbyrne@gmail.com>
2015-07-15 16:39:55 +0100

    Revert "commit_work_37"
    This reverts commit 2981f74fe4fe7931fbd0724393668ba006e1fd9e.

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,13 +1,34 @@
 angular.module('starter.directives', [])
 
-.directive('onErrorSrc', function() {
+.directive('map', function() {
   return {
-    link: function(scope, element, attrs) {
-      element.bind('error', function() {
-        if (attrs.src != attrs.onErrorSrc) {
-          attrs.$set('src', attrs.onErrorSrc);
-        }
-      });
+    restrict: 'E',
+    scope: {
+      onCreate: '&'
+    },
+    link: function ($scope, $element, $attr) {
+      function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(43.07493, -89.381388),
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map($element[0], mapOptions);
+  
+        $scope.onCreate({map: map});
+
+        // Stop the side bar from dragging when mousedown/tapdown on the map
+        google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
+          e.preventDefault();
+          return false;
+        });
+      }
+
+      if (document.readyState === "complete") {
+        initialize();
+      } else {
+        google.maps.event.addDomListener(window, 'load', initialize);
+      }
     }
   }
 });
